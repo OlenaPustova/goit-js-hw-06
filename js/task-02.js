73141aedfcfa05915e352a66850b8f32a35d2650
OlenaPustova <i.am.elena.p@gmail.com>
2022-02-23 21:56:50 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,19 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ulIngredients = document.querySelector("#ingredients");
+
+const arrLi = ingredients.map((item) => {
+  const ingredient = document.createElement("li");
+  ingredient.classList.add("item");
+  ingredient.textContent = item;
+  return ingredient;
+});
+
+ulIngredients.append(...arrLi);
