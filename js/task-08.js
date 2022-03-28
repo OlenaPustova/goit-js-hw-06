9fa55825f65adf8d3e389489b86cdeb8af90fea7
OlenaPustova <i.am.elena.p@gmail.com>
2022-03-28 23:15:49 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const formEl = document.querySelector(".login-form");
 
+formEl.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const formElements = event.currentTarget.elements;
+
+  if (!formElements.email.value || !formElements.password.value) {
+    alert("Всі поля повинні бути заповнені");
+  } else {
+    const mail = formElements.email.value;
+    const password = formElements.password.value;
+    const formData = {
+      mail,
+      password,
+    };
+
+    console.log(formData);
+
+    formEl.reset();
+  }
+});
